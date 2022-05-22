314124d659d1baf9bbf3f4363ccea449b3bf8ab4
DANIEL <yuntaehui77@gmail.com>
2022-05-22 20:04:27 +0900

    localforage, vlf 제거

--- README.md
+++ README.md
@@ -4,7 +4,10 @@
 > > - eslint, pretteir 설정
 > > - vue-router 설치 
 > >   - npm i vue-router@3.5.3
-> > - 스토리지 선택 indexedDB
-> >   - npm i localforage
-> >   - npm i --save localforage vlf
-> >   - 캡틴판교 앤드게임 회원가입 localforage 전환
+> > - ~~스토리지 선택 indexedDB~~
+> >   - ~~npm i localforage~~
+> >   - ~~npm i --save localforage vlf~~
+> >   - ~~캡틴판교 앤드게임 회원가입 localforage 전환~~
+> > - 스토리지 선택 Local Storage로 진행
+> >   - localforage, vlf 제거(indexedDB의 러닝커브가 높음)
+> >   - indexedDB는 다음 기회에...
--- src/main.js
+++ src/main.js
@@ -2,21 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from '@/routes/index'
-import localforage from 'localforage'
-import Vlf from 'vlf'
-
-Vue.use(Vlf, localforage)
 
 Vue.config.productionTip = false
-
-localforage.config({
-  driver: localforage.WEBSQL, // Force WebSQL; same as using setDriver()
-  name: 'myApp',
-  version: 1.0,
-  size: 4980736, // Size of database, in bytes. WebSQL-only for now.
-  storeName: 'keyvaluepairs', // Should be alphanumeric, with underscores.
-  description: 'some description',
-})
 
 new Vue({
   router,
