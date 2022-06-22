63389443ac45e67296b465a4099910caf989be7b
DANIEL <yuntaehui77@gmail.com>
2022-06-22 23:05:26 +0900

    2022-06-22

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,27 +1,35 @@
 <template>
   <el-table
+    ref="table"
     :data="tableData"
     style="width: 100%"
     :row-class-name="tableRowClassName"
+    @selection-change="handleSelectionChange"
   >
     <el-table-column prop="date" label="Date" width="180" />
     <el-table-column prop="name" label="Name" width="180" />
     <el-table-column prop="address" label="Address" />
+    <el-table-column label="선택하세요">
+      <template>
+        <el-select
+          v-model="value"
+          placeholder="Select"
+          @change="changeSelect($event)"
+        >
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          />
+        </el-select>
+      </template>
+    </el-table-column>
   </el-table>
 </template>
 
 <script>
 export default {
-  methods: {
-    tableRowClassName({ row, rowIndex }) {
-      if (rowIndex === 1) {
-        return 'warning-row'
-      } else if (rowIndex === 3) {
-        return 'success-row'
-      }
-      return ''
-    },
-  },
   data() {
     return {
       tableData: [
@@ -46,7 +54,49 @@ export default {
           address: 'No. 189, Grove St, Los Angeles',
         },
       ],
+      options: [
+        {
+          value: 'Option1',
+          label: 'Option1',
+        },
+        {
+          value: 'Option2',
+          label: 'Option2',
+        },
+        {
+          value: 'Option3',
+          label: 'Option3',
+        },
+        {
+          value: 'Option4',
+          label: 'Option4',
+        },
+        {
+          value: 'Option5',
+          label: 'Option5',
+        },
+      ],
+      value: '',
+      multipleSelection: [],
     }
+  },
+  methods: {
+    tableRowClassName({ rowIndex }) {
+      if (rowIndex === 1) {
+        return 'warning-row'
+      } else if (rowIndex === 3) {
+        return 'success-row'
+      }
+      return ''
+    },
+    handleSelectionChange(val) {
+      this.multipleSelection = val
+      console.log(`row: ${this.multipleSelection}, val: ${val}`)
+    },
+    changeSelect(event) {
+      this.$refs.table.toggleRowSelection(event)
+      console.log(`${event}`)
+    },
   },
 }
 </script>
